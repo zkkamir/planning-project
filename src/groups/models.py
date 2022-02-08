5d0e0fed56d2858f0a4faac650e01dd7e7a0e5de
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2022-02-08 16:02:31 +0330

    Added the group model.

--- src/groups/models.py
+++ src/groups/models.py
@@ -1,1 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
+
+
+class Group(models.Model):
+    """
+    A class to represent a group of tasks.
+    """
+
+    name = models.CharField(_("group name"), max_length=127)
+    description = models.TextField(_("group description"))
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, verbose_name=_("owner")
+    )
+    slug = models.SlugField(_("group slug"), editable=False, unique=True)
+
+    class Meta:
+        unique_together = (
+            "name",
+            "owner",
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
