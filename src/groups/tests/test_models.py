073b62642b6d0c571ef7995c7d71124d63785392
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2022-02-08 16:02:01 +0330

    Modified the group model tests.

--- src/groups/tests/test_models.py
+++ src/groups/tests/test_models.py
@@ -1,22 +1,31 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
 from groups.models import Group
 
 
 @pytest.mark.django_db
-def test_create_group():
+@pytest.mark.parametrize(
+    "name, description",
+    [
+        ("Test Name", "Test Description"),
+        ("teST_name", "teST_description"),
+        ("TEST NAME", "TEST DESCRIPTION"),
+    ],
+)
+def test_create_group(name, description):
     """
     Test group instance creation.
     """
-    user = get_user_model.objects.create_user(
+    user = get_user_model().objects.create_user(
         email="test@test.test", password="testtest"
     )
     group = Group.objects.create(
-        name="test name", description="test description", owner=user
+        name=name, description=description, owner=user
     )
 
-    assert group.name == "test name"
-    assert group.description == "test description"
-    assert group.user == user
-    assert group.slug == "test-name"
+    assert group.name == name
+    assert group.description == description
+    assert group.owner == user
+    assert group.slug == slugify(name)
