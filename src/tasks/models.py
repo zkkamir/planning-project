234c4aa5793556625fa4fdea3a7bc071416f906b
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2022-02-10 13:50:11 +0330

    Added task model, hacked tests to work for now.

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from groups.models import Group
+
+
+class Task(models.Model):
+    """
+    A class to represent a task. tasks can be placed in different groups and
+    convey different meanings.
+    """
+
+    class Tags(models.IntegerChoices):
+        """Choices available for task's tag field."""
+
+        NI = 1, "Not Important"
+
+    subject = models.CharField(_("task subject"), max_length=255)
+    notes = models.TextField(_("task notes"), blank=True, null=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        related_name="tasks",
+        on_delete=models.CASCADE,
+        verbose_name=_("task owner"),
+    )
+    slug = models.SlugField(_("task slug"))
+    parent = models.ForeignKey(
+        "self",
+        related_name="children",
+        on_delete=models.CASCADE,
+        verbose_name=_("task parrent"),
+        blank=True,
+        null=True,
+    )
+    start_date = models.DateField(blank=True, null=True)
+    finish_date = models.DateField(blank=True, null=True)
+    start_time = models.DateTimeField(blank=True, null=True)
+    finish_time = models.DateTimeField(blank=True, null=True)
+    tag = models.PositiveSmallIntegerField(
+        _("tag"), choices=Tags.choices, blank=True, null=True
+    )
+    reason = models.CharField(_("why this has to be done"), max_length=255)
+    groups = models.ManyToManyField(
+        Group,
+        related_name="items",
+        blank=True,
+        verbose_name=_("groups"),
+    )
+
+    @property
+    def has_children(self):
+        return bool(self.children.all().count())
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.subject)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.subject
--- src/tasks/tests/test_models.py
+++ src/tasks/tests/test_models.py
@@ -3,45 +3,52 @@ import pytest
 from django.utils.text import slugify
 
 
-from groups.models import Group
 from tasks.models import Task
 
 
 @pytest.mark.parametrize(
     "kwargs",
     [
-        ({"subject": "test"},),
-        ({"subject": "test", "notes": "test note"},),
-        (
-            {
-                "subject": "test",
-                "notes": "test note",
-                "start_date": date.today(),
-            },
-        ),
-        (
-            {
-                "subject": "test",
-                "notes": "test note",
-                "start_date": date.today(),
-                "finish_date": date.today(),
-                "group": "test",
-            },
-        ),
+        {"subject": "test"},
+        {"subject": "test", "notes": "test note"},
+        {
+            "subject": "test",
+            "notes": "test note",
+            "start_date": date.today(),
+        },
+        {
+            "subject": "test",
+            "notes": "test note",
+            "start_date": date.today(),
+            "finish_date": date.today(),
+            "group": "test",
+        },
     ],
 )
 @pytest.mark.django_db
-def test_creating_task_successful(db, user, django_user_model, **kwargs):
+def test_creating_task_successful(user, kwargs):
     """
     Test the happy path of Task instance creation.
     """
     kwargs["owner"] = user
-    # Not clean, try to clean up
+    group_name = None
+    # Does not seem clean, try to clean up
     if "group" in kwargs:
-        kwargs["group"] = Group(name=kwargs["group"], owner=user)
-    task = Task.objects.create(**kwargs)
+        group_name = kwargs.pop("group")
 
-    assert task.slug == slugify(kwargs["name"])
+    task = Task.objects.create(**kwargs)
+    task.save()
+    # Does not seem clean, try to clean up
+    if group_name:
+        task.groups.create(name=group_name, owner=user)
+
+    assert task.slug == slugify(kwargs["subject"])
     assert task.has_children is False
     assert task == user.tasks.get(id=task.id)
-    assert task == kwargs["group"].items.get(id=task.id)
+    # Does not seem clean, try to clean up
+    if task.groups.all().count():
+        if task.groups.all().count() > 1:
+            for group in task.groups.all():
+                assert task == group.items.get(id=task.id)
+        else:
+            assert task == task.groups.all()[0].items.get(id=task.id)
